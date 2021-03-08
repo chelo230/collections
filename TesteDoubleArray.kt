961ff947c4ad1954ecaf923de66ec6a00cad4bd5
chelo230 <65545707+chelo230@users.noreply.github.com>
2021-03-08 20:38:00 -0300

    Update TesteDoubleArray.kt
    Commit com exemplo double array.

--- TesteDoubleArray.kt
+++ TesteDoubleArray.kt
@@ -10,6 +10,16 @@ fun main() {
     salarios.forEach { println(it) }
 
     println("---------------- 2 - Forma Double Array  -----------")
-    salarios.forEachIndexed
+    salarios.forEachIndexed { index: Int, salario: Double ->
+
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("---------------- 3 - Forma Double Array ordenado -----------")
+    val salarios2 = doubleArrayOf(1500.0, 1200.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
 
 }
+
